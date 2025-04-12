a9c3c9352106368d067a773a1c3069cc47cd70ff
Priyanshu suryawanshi <132843870+priyanshu1344@users.noreply.github.com>
2025-04-12 23:25:14 +0530

    Update Songs.js

--- Songs.js
+++ Songs.js
@@ -5,7 +5,7 @@ import { playlistState } from "../atoms/playlistAtom";
 import Song from "./Song";
 
 const Songs = () => {
-  const playlist = useRecoilValue(playlistState);
+  const playlist = useRecoilValue(playlistState); 
 
   return (
     <div className="px-8 flex flex-col space-y-1 pb-28 text-white">
